3d25fb3d703d3d64ec6eb2ca5245607482c6cbf2
walkerdrury <walkerdrury@tamu.edu>
2022-10-31 13:31:01 -0500

    database connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 const express   = require('express');
 const app       = express();
+const { Pool } = require('pg');
+const dotenv = require('dotenv').config();
 
 app.use(express.json());
+
+const pool = new Pool({
+    user: process.env.PSQL_USER,
+    host: process.env.PSQL_HOST,
+    database: process.env.PSQL_DATABASE,
+    password: process.env.PSQL_PASSWORD,
+    port: process.env.PSQL_PORT,
+    ssl: {rejectUnauthorized: false}
+});
 
 // ex: http://localhost:4000/test-connection
 app.get('/test-connection', (req, res, next) => { 
@@ -13,9 +24,6 @@ app.get('/say-name/:name', (req, res, next) => {
     const {name} = req.params;
     res.send("hi " + name);
 });
-
-
-
 
 
 
