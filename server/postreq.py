298570ac89bde816c46521b20fec95dfaeceefe1
walkerdrury <walkerdrury@tamu.edu>
2022-11-20 20:53:20 -0600

    added restock report

--- server/postreq.py
+++ server/postreq.py
@@ -1,5 +1,5 @@
 import requests
 
-requests.post("http://localhost:4000/change-minimumamount", json={'name': 'pumpkin spice', 'minimumamount': 200})
-#a = requests.get("http://localhost:4000/get-inventory")
-#print(a.text)
+#requests.post("http://localhost:4000/set-price", json={'name': 'potato2', 'price': 10})
+a = requests.get("http://localhost:4000/restock-report")
+print(a.text)
