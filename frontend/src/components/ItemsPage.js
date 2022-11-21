b77e03662999ab2b2449f261c1aeddfca34c4832
Sammyreel <sammyreel@tamu.edu>
2022-11-20 23:13:00 -0600

    Add menu items almost function, working on bugs

--- frontend/src/components/ItemsPage.js
+++ frontend/src/components/ItemsPage.js
@@ -1,21 +1,24 @@
 import React from "react";
 import ManagerTopbar from "./ManagerTopbar";
 import "./ItemsPage.css";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Popup from "./Popup";
 
 const ItemsPage = () => {
     const [error, setError] = useState(false);
     const [popup, setPopup] = useState(false);
+    const [popup2, setPopup2] = useState(false);
     const [allItems, setAllItems] = useState([]);
     const [refresh, setRefresh] = useState(false);
     const [waiting, setWaiting] = useState(false);
-    const newItemCost = useRef("");
-    const newItemName = useRef("");
-    const newItemType = useRef("");
-    const newItemIngredientsString = useRef("");
+    const [title, setTitle] = useState("");
+    const [newItemCost, setNewItemCost] = useState("");
+    const [newItemName, setNewItemName] = useState("");
+    const [newItemType, setNewItemType] = useState("");
+    const [newItemIngredientsString, setNewItemIngredientsString] = useState("");
     const [newItemIngredientsList, setNewItemIngredientsList] = useState([]);
+    const [found, setFound] = useState(false);
 
     const callApi = async () => {
         setWaiting(true)
@@ -26,6 +29,11 @@ const ItemsPage = () => {
         setWaiting(false)
         return;
     };
+
+    useEffect(() => {
+        callApi();
+    }, [refresh]);
+
 
 
     const removeItem = async (itemname) => {
@@ -48,45 +56,77 @@ const ItemsPage = () => {
 
     const reset = () => {
         setPopup(false);
-        newItemCost.current.valueOf = "";
-        newItemName.current.valueOf = "";
-        newItemType.current.valueOf = "";
-        newItemIngredientsString.current.valueOf = "";
+        setPopup2(false);
+        setError(false);
+        //setFound(false);
+        setNewItemCost("");
+        setNewItemName("");
+        setNewItemType("");
+        setNewItemIngredientsString("");
     };
 
-    const addItem =  async () => {
-        let found = false;
-        console.log("Checking if ", newItemName.current.value, " already exists");
-        allItems.map(async (item) => {
-            if (item.name === newItemName.current.value) {
-                console.log(newItemName.current.value, " already exists, changing price to ", newItemCost.current.value, " and type to ", newItemType.current.value);
-                if (item.onmenu === "no") {
-                    await axios.post("https://tyson-express.onrender.com/toggle-item", {name: item.name});
-                }
-                await axios.post("https://tyson-express.onrender.com/set-price", {name: item.name, cost: newItemCost.current.value});
-                await axios.post("https://tyson-express.onrender.com/change-type", {name: item.name, type: newItemType.current.value});
-                await axios.post("https://tyson-express.onrender.com/change-type", {name: item.name, ingredients: newItemIngredientsList});
-                found = true;
-                return;
+    const addOldItem = async (item) => {
+        let result;
+        if (item.onmenu === "no") {
+            result = await axios.post("https://tyson-express.onrender.com/toggle-item", {name: item.name});
+            console.log("On menu: ", result);   
+        }
+        await axios.post("https://tyson-express.onrender.com/set-price", {name: item.name, price: newItemCost});
+        result = await axios.post("https://tyson-express.onrender.com/change-type", {name: item.name, type: newItemType});
+        console.log(result)
+        await axios.post("https://tyson-express.onrender.com/change-type", {name: item.name, ingredients: newItemIngredientsList});
+    }
+
+    const addNewItem = async () => {
+        let result = await axios.post("https://tyson-express.onrender.com/add-item", {name: newItemName, price: newItemCost, type: newItemType, ingredients: newItemIngredientsList});
+        console.log(result);
+    }
+
+    const changePrice = async () => {
+        if (newItemCost === "") {
+            setError(true)
+            return
+        }
+
+        setError(false);
+        setWaiting(true);
+
+        console.log("Changing price of ", title, " to ", Number(newItemCost));
+
+        let result = await axios.post("https://tyson-express.onrender.com/set-price", {name: title, price: newItemCost});
+
+        console.log(result);
+        setPopup2(false);
+        reset();
+        setRefresh(!refresh);
+    }
+
+    const checkForItem = async () => {
+        console.log("Checking if ", newItemName.toLowerCase(), " already exists");
+        await allItems.map((item) => {
+            if (item.name === newItemName.toLowerCase()) {
+                setFound(true);
+                addOldItem(item);
             }
+            return;
         })
 
         if (!found) {
-            await axios.post("https://tyson-express.onrender.com/add-item", {name: newItemName, cost: newItemCost, type:newItemType, ingredients:newItemIngredientsList});
+            console.log("in wrong function")
+            addNewItem();
         }
     }
-
     const getItemInfo = async () => {
-        if (newItemCost.current.value === "" 
-            || newItemName.current.value === "" 
-            || newItemType.current.value === "" 
-            || newItemIngredientsString.current.value === "") {
+        if (newItemCost === "" 
+            || newItemName === "" 
+            || newItemType === "" 
+            || newItemIngredientsString === "") {
             setError(true);
             return;
         }
         setError(false);
         setWaiting(true);
-        setNewItemIngredientsList(newItemIngredientsString.current.value.split(','));
+        setNewItemIngredientsList(newItemIngredientsString.split(','));
         
         newItemIngredientsList.forEach(function(part, index) {
             this[index] = part.trim();
@@ -94,10 +134,11 @@ const ItemsPage = () => {
 
         console.log(newItemIngredientsList);
 
-        addItem(newItemName);
+        await checkForItem(newItemName);
 
-        console.log("Name: ", newItemName.current.value, " Cost: ", newItemCost.current.value, " Type: ", newItemType.current.value);
+        console.log("Name: ", newItemName, " Cost: ", newItemCost, " Type: ", newItemType);
         setRefresh(!refresh);
+        reset();
         setPopup(false);
     }
 
@@ -116,7 +157,38 @@ const ItemsPage = () => {
         }
     };
 
-    const popUp = (type, index, ingredient) => {
+    const popUp2 = () => {
+        if (popup2) {
+            return (
+                <Popup setPopup={setPopup2}>
+                    <h2 className='to-center'>Change price of {title.replace(
+                                        /(^\w{1})|(\s+\w{1})/g,
+                                        (letter) => letter.toUpperCase()
+                                        )}</h2>
+                    <div className="ui form">
+                        <div className="field">
+                            <label></label>
+                            <input type="number" 
+                                placeholder="Enter Item Cost"  
+                                value={newItemCost}
+                                onChange={(event) => {setError(false); setNewItemCost(event.target.value);}}>
+                            </input>
+                        </div>
+                    </div>
+                    <br />
+            <div className='to-center'>
+                        <button className='ui red button' onClick={reset}>
+                            Cancel
+                        </button>
+                        <button className='ui green button' onClick={() => changePrice()}>Submit</button>
+                    </div>
+                    <div>{displayError()}</div>
+                </Popup>
+            );
+        }
+    }
+
+    const popUp = () => {
         if (popup) {
             return (
                 <Popup setPopup={setPopup}>
@@ -127,21 +199,21 @@ const ItemsPage = () => {
                         <label>Item Name</label>
                         <input type="text" 
                             placeholder="Enter Item Name" 
-                            ref={newItemName} 
-                            onChange={() => setError(false)}>
+                            value={newItemName} 
+                            onChange={(event) => {setError(false); setNewItemName(event.target.value)}}>
                         </input>
                     </div>
                     <div className="field">
                         <label>Cost</label>
                         <input type="number" 
                             placeholder="Enter Item Cost" 
-                            ref={newItemCost} 
-                            onChange={(e) => setError(false)}>
+                            value={newItemCost}
+                            onChange={(event) => {setError(false); setNewItemCost(event.target.value)}}>
                         </input>
                     </div>
                     <div className="field">
                         <label>Item Type</label>
-                        <select className="ui fluid dropdown" ref={newItemType} onChange={() => setError(false)}>
+                        <select className="ui fluid dropdown" value={newItemType} onChange={(event) => {setError(false); setNewItemType(event.target.value);}}>
                             <option value="">Select Item Type</option>
                             <option value="entree">Entree</option>
                             <option value="side">Side</option>
@@ -151,7 +223,7 @@ const ItemsPage = () => {
                 </div>
                 <div className="field">
                     <label>Ingredients</label>
-                    <textarea placeholder="Enter ingredients as a comma-separated list (i.e. beef, broccoli, beef and broccoli sauce)" ref={newItemIngredientsString} onChange={() => setError(false)}></textarea>
+                    <textarea placeholder="Enter ingredients as a comma-separated list (i.e. beef, broccoli, beef and broccoli sauce)" value={newItemIngredientsString} onChange={(event) => {setError(false); setNewItemIngredientsString(event.target.value)}}></textarea>
                 </div>
             </form>
             <br />
@@ -219,7 +291,7 @@ const ItemsPage = () => {
                                     </h5>
                                 </div>
                                 <div className='to-center'>
-                                    <button class="ui blue button">
+                                    <button class="ui blue button" onClick={() => {setTitle(item.name); setPopup2(true);}}>
                                         Change Price
                                     </button>
                                     <button className="negative ui button" onClick={() => removeItem(item.name)}>
@@ -249,6 +321,7 @@ const ItemsPage = () => {
     return (
         <div>
             {popUp()}
+            {popUp2()}
             <ManagerTopbar />
             <br />
             <h1 className='to-center'>Menu Items</h1>
