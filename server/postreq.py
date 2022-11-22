e3f582bab59d86c9c41e7540470fc5f49210b6b8
walkerdrury <walkerdrury@tamu.edu>
2022-11-21 18:57:15 -0600

    get-ingredients added

--- server/postreq.py
+++ server/postreq.py
@@ -1,5 +1,7 @@
 import requests
 
-requests.post("http://localhost:4000/set-price", json={'name': 'potato2', 'price': '12'})
-#a = requests.get("http://localhost:4000/excess-report/01-01-2022")
-#print(a.text)
+#url = "http://localhost:4000"
+url = "https://tyson-express.onrender.com"
+#requests.post(url + "/change-type", json={'name': 'potato3', 'type': 'pen15'})
+a = requests.get("http://localhost:4000/get-ingredients/orange chicken")
+print(a.text)
