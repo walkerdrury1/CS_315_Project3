4f296413c77c0317570370d1db931a7a438a4363
Pranav <pranav123@tamu.edu>
2022-12-06 18:38:57 -0600

    Finished oauth

--- server/index.js
+++ server/index.js
@@ -185,6 +185,9 @@ app.use('/', menuitemsRouter)
 const reportRouter = require('./reports')
 app.use('/', reportRouter)
 
+const oauthRouter = require('./oauth')
+app.use('/', oauthRouter)
+
 app.listen(PORT, function () {
     console.log('Server is running on port ' + PORT);
 });
